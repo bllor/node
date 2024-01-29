8076a9aab8d285e8bca6497235772486410cefd4
bllor <duki0105@gmail.com>
2024-01-30 07:42:29 +0900

    1/30

---
+++ syntax/string.js
@@ -0,0 +1,9 @@
+// 문자열은 "" 또는 ''으로 표현하면 되며,
+// 시작과 끝이 같아야 한다.
+
+console.log('1'+'1');
+//문자와 문자 사이에 +가 있으면 더하지 않고, 문자열을 합친다.
+
+var str = 'rewfadsfqeasfedskfjhesdkjfhaksdjfhaljewhflkjewhfjkewf' ;
+console.log(str.length);
+
---
+++ syntax/template.js
@@ -0,0 +1,14 @@
+var name = 'dongil';
+
+var str = name+'rewfadsfqeasfedskfjhesdkjfhaksdjfhaljewhflkjewhfjkewf' ;
+console.log(str);
+
+// 줄바꿈을 하는 특수 문자 : \n
+// 리터럴이란? 정보를 표현하는 기법
+// ex) var a = 1; 1은 숫자라는 정보를 표현
+
+var str2 = 'dear'+name+'rewfadsfqeasfe\ndskfjhesdkjfhaksdjfhaljewhflkjewhfjkewf' ;
+var str3 = `dear${name}rewfadsfqeasfe
+dskfjhesdkjfhaksdjfhaljewhflkjewhfjkewf` ;
+console.log(str2);
+console.log(str3);
