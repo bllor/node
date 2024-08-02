71b8a5ec68f091ce7c8271799d1fbf05d768bf25
bllor <duki0105@gmail.com>
2024-08-03 03:30:49 +0900

    조건문

---
+++ egoing/syntax/conditional.js
@@ -0,0 +1,20 @@
+var args = process.argv;
+console.log(args[2])
+/*
+console.log(args) 출력시 
+[
+  '/usr/local/bin/node', //node런타임의 위치
+  '/Users/bia/Desktop/workspace/node/egoing/syntax/conditional.js', // 파일의 위치
+  'hi' // 출력값
+]
+*/
+
+/*
+if (true){
+ print(true)
+}else{
+ print(false)
+}
+조건문에 true가 들어갈 경우, if 안의 내용이 출력된다.
+
+*/
